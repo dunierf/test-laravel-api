0279f5fd32a0862ffba1004e029e9aa66bb3934a
Duniesqui Rodriguez <dunierf@gmail.com>
2024-01-15 20:18:18 -0800

    Swagger docu

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,18 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *    title="Api",
+ *    description="RESTful API using Laravel",
+ *    version="1.0.0",
+ *    @OA\Contact(
+ *           email="dunierf@gmail.com"
+ *    ),
+ * )
+ *
+ */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
