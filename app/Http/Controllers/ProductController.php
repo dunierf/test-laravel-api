a15a42dccb4dd8d69d853f749cf614230fe6bbe0
Duniesqui Rodriguez <dunierf@gmail.com>
2024-01-15 22:57:21 -0800

    Defining product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * @OA\Get(
+     *      path="/api/products",
+     *      tags={"Products"},
+     *      summary="Get products collection",
+     *      description="Get a list of products",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(property="data", type="array", @OA\Items(ref="#/components/schemas/ProductDtoResource"))
+     *          )
+     *       ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Not found"
+     *      )
+     * )
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * @OA\Post(
+     *      path="/api/products",
+     *      tags={"Products"},
+     *      summary="Create a product",
+     *      description="Create a product",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="Object",
+     *          @OA\JsonContent(ref="#/components/schemas/ProductDtoResource")
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Created product",
+     *          @OA\JsonContent(ref="#/components/schemas/ProductDtoResource")
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized"
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      )
+     * )
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * @OA\Get(
+     *      path="/api/products/{id}",
+     *      tags={"Products"},
+     *      summary="Get a product",
+     *      description="Get a product by id",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer",
+     *              example=1
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/ProductDtoResource")
+     *       ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Not found"
+     *      )
+     * )
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * @OA\Put(
+     *      path="/api/products/{id}",
+     *      tags={"Products"},
+     *      summary="Update a product",
+     *      description="Update a product",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer",
+     *              example=1
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="Object",
+     *          @OA\JsonContent(ref="#/components/schemas/ProductDtoResource")
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Updated user",
+     *          @OA\JsonContent(ref="#/components/schemas/ProductDtoResource")
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized"
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      )
+     * )
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * @OA\Delete(
+     *      path="/api/products/{id}",
+     *      tags={"Products"},
+     *      summary="Delete a product",
+     *      description="Delete a product by id",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer",
+     *              example=1
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=204,
+     *          description="No Content"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized"
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Not found"
+     *      )
+     * )
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
---
+++ app/Http/Resources/ProductDtoResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @OA\Schema(
+ *     title="Product",
+ *     description="Product Dto",
+ *     required={"name", "email"},
+ *     properties={
+ *         @OA\Property(property="id", type="int64", example="1"),
+ *         @OA\Property(property="name", type="string", example="Admin"),
+ *         @OA\Property(property="price", type="decimal", example="12.30"),
+ *         @OA\Property(property="urlImg", type="string", example="http://www.domain.com/path/to/image.jpg")
+ *     }
+ * )
+ */
+class ProductDtoResource extends JsonResource
+{
+    public static $wrap = null;
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'     => $this->id,
+            'name'   => $this->name,
+            'price'  => $this->price,
+            'urlImg' => $this->url_img
+        ];
+    }
+}
