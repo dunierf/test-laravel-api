ebab05e74a6cb2245abd6f8fc225b3098cd33006
Duniesqui Rodriguez <dunierf@gmail.com>
2024-01-16 11:31:47 -0800

    Creating a user

--- app/Http/Middleware/HasRoleMiddleware.php
+++ app/Http/Middleware/HasRoleMiddleware.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Models\Role;
+use App\Exceptions\InsufficientPrivilegesException;
 
 class HasRoleMiddleware
 {
@@ -15,6 +17,14 @@ class HasRoleMiddleware
      */
     public function handle(Request $request, Closure $next, string $role): Response
     {
+        $role = Role::where('name', $role)->first();
+
+        if (is_null($role))
+            throw new InsufficientPrivilegesException();
+
+        if  (!$request->user()->roles()->where('role_id', $role->id)->count())
+            throw new InsufficientPrivilegesException();
+
         return $next($request);
     }
 }
